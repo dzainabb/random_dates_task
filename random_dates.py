bb911211dd6c05117a88cf4118fe21b77577eb2b
dzainabb <zainabdar24@yahoo.com>
2025-11-12 00:21:41 +0000

    completeing dates calculator using csv file

--- random_dates.py
+++ random_dates.py
@@ -3,10 +3,26 @@
 import numpy as np
 import pandas as pd
 
-random_dates = pd.read_csv('/Users/sabiqadar/Downloads/random_dates.csv')
-print(random_dates.head(3))
+random_dates = pd.read_csv('random_dates.csv')
+print(random_dates.head(1))
+
+random_dates = random_dates.iloc[:,0].values.astype('datetime64[D]')
 
 
 date_today = np.datetime64('today', 'D')    
 print("Today's date:")
 print(date_today)
+
+date_difference = date_today - random_dates
+
+result =pd.DataFrame(
+    {
+    'random_date' : random_dates,
+    'difference_in_days' : date_difference
+})
+
+print(result)
+result.to_csv('date_differences.csv', index=False)
+
+
+
