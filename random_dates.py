08fd73234ccb61c9c50cc928b6d0cf8f4f46eaff
dzainabb <zainabdar24@yahoo.com>
2025-12-06 23:01:55 +0000

    moving from local repo - isses with gitnore

--- random_dates.py
+++ random_dates.py
@@ -3,18 +3,21 @@
 import numpy as np
 import pandas as pd
 
+# Load random dates from CSV file
 random_dates = pd.read_csv('random_dates.csv')
 print(random_dates.head(1))
 
+# Convert to datetime64[D] format
 random_dates = random_dates.iloc[:,0].values.astype('datetime64[D]')
 
-
+# Get today's date
 date_today = np.datetime64('today', 'D')    
 print("Today's date:")
 print(date_today)
 
 date_difference = date_today - random_dates
 
+# Create a DataFrame to store results
 result =pd.DataFrame(
     {
     'random_date' : random_dates,
